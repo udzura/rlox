961e6e2d1b666a597f84eec20b29d616652ef78a
Uchio Kondo <udzura@udzura.jp>
2021-10-19 01:18:38 +0900

    scanning: First code

--- rlox-part1/src/main.rs
+++ rlox-part1/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::{env::args, process::exit};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    match args().len() {
+        3..=usize::MAX => {
+            println!("Usage: rlox [script]");
+            exit(64);
+        }
+        2 => {
+            // run_file(args().nth(1));
+            unimplemented!("file");
+        }
+        _ => {
+            // run_prompt();
+            unimplemented!("prompt");
+        }
+    }
 }
