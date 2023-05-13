31ffa8898fef5742d4e8b1db810fba66d911b5e0
Uchio Kondo <udzura@udzura.jp>
2023-05-13 20:42:47 +0900

    Remove old example

--- rlox-II/examples/ast_printer.rs
+++ rlox-II/examples/ast_printer.rs
@@ -1,7 +1,7 @@
 extern crate rlox_ii;
-use rlox_ii::ast_printer::AstPrinter;
-use rlox_ii::expr::*;
-use rlox_ii::token::*;
+// use rlox_ii::ast_printer::AstPrinter;
+// use rlox_ii::expr::*;
+// use rlox_ii::token::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let expression = Expr::binary(
@@ -9,17 +9,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //     Token::new(TokenType::PLUS, "+", Literal::Nil, 1),
     //     Expr::literal(Literal::Num(3.0)),
     // );
-    let nil = Literal::Nil;
+    // let nil = Literal::Nil;
 
-    let expression = Expr::binary(
-        Expr::unary(
-            Token::new(TokenType::MINUS, "-", nil.clone(), 1),
-            Expr::literal(Literal::Num(123.0)),
-        ),
-        Token::new(TokenType::STAR, "*", nil.clone(), 1),
-        Expr::grouping(Expr::literal(Literal::Num(45.67))),
-    );
+    // let expression = Expr::binary(
+    //     Expr::unary(
+    //         Token::new(TokenType::MINUS, "-", nil.clone(), 1),
+    //         Expr::literal(Literal::Num(123.0)),
+    //     ),
+    //     Token::new(TokenType::STAR, "*", nil.clone(), 1),
+    //     Expr::grouping(Expr::literal(Literal::Num(45.67))),
+    // );
 
-    println!("{}", AstPrinter {}.print(&expression));
+    // println!("{}", AstPrinter {}.print(&expression));
     Ok(())
 }
